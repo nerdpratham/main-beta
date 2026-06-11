44f817c995cfc2881cf46783fa7af1c589c44889
nerdpratham <pratham100yadav@gmail.com>
2026-06-11 11:25:53 +0530

    Updates from Ship Studio

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
   plugins: [
     react(),
     tailwindcss(),
